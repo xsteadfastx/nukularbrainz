1605b37a33674563a2bc58a9bfd619ba09dab64d
Marvin Preuss <marvin@xsteadfastx.org>
2026-08-10 17:19:46 +0200

    feat: add yambs MusicBrainz seeder package

--- flake.nix
+++ flake.nix
@@ -15,6 +15,22 @@
       let
         pkgs = inputs.nixpkgs.legacyPackages.${system};
 
+        yambs = pkgs.buildGoModule {
+          pname = "yambs";
+          version = "0.1.15";
+          src = pkgs.fetchgit {
+            url = "https://codeberg.org/derat/yambs.git";
+            rev = "v0.1.15";
+            sha256 = "sha256-gzyRLOi0IWIgr+ITx3njfkMFiQ0D6XyHkM0EqStlQqs=";
+          };
+          subCmds = [
+            "yambs"
+          ];
+          vendorHash = "sha256-OmxthdcVLsm8tkizrqFegAIgs7oRVFgW25OnbCkwiAU=";
+          # render tests call out to W3C validators (no network in sandbox)
+          doCheck = false;
+        };
+
         # Generate pre-commit hooks with extras
         preCommitGen = inputs.pre-commit.lib.generate {
           inherit pkgs system;
@@ -30,6 +46,7 @@
 
       in
       {
+        packages.default = yambs;
         checks.pre-commit-check = preCommitGen.pre-commit-check;
         inherit (preCommitGen) formatter;
         devShells.default = preCommitGen.devShell;
